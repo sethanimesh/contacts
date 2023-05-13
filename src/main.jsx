c89319c00c7082bda1b232d03179192ebc7948fe
Animesh Seth <sethanimesh@hotmail.com>
2023-05-13 14:46:13 +0530

    Dyanamic contact list with new button function

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,10 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import './index.css'  
-import Root from "./routes/root";
 import ErrorPage from "./error-page";
 import Contact from "./routes/contact";
+import Root, { loader as rootLoader, action as rootAction,
+} from "./routes/root";
 
 
 const router = createBrowserRouter([
@@ -15,6 +16,9 @@ const router = createBrowserRouter([
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+
     children: [
       {
         path: "contacts/:contactId",
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,7 +1,13 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useLoaderData, Form} from "react-router-dom";
+import { getContacts, createContact } from "../contacts";
 
+export async function action() {
+  const contact = await createContact();
+  return { contact };
+}
 
 export default function Root() {
+    const { contacts } = useLoaderData();
     return (
       <>
         <div id="sidebar">
@@ -25,20 +31,35 @@ export default function Root() {
                 aria-live="polite"
               ></div>
             </form>
-            <form method="post">
-              <button type="submit">New</button>
-            </form>
+            <Form method="post">
+            <button type="submit">New</button>
+          </Form>
           </div>
           <nav>
+            {contacts.length ? (
             <ul>
-              <li>
-              <Link to={`contacts/1`}>Animesh Seth</Link>
-              </li>
-              <li>
-              <Link to={`contacts/2`}>Max Wilson</Link>
-              </li>
+              {contacts.map((contact) => (
+                <li key={contact.id}>
+                  <Link to={`contacts/${contact.id}`}>
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No Name</i>
+                    )}{" "}
+                    {contact.favorite && <span>★</span>}
+                  </Link>
+                </li>
+              ))}
             </ul>
+          ) : (
+            <p>
+              <i>No contacts</i>
+            </p>
+          )}
           </nav>
+        
         </div>
         <div id="detail">
             <Outlet />
@@ -46,3 +67,10 @@ export default function Root() {
       </>
     );
   }
+
+
+  export async function loader() {
+    const contacts = await getContacts();
+    return { contacts };
+  } 
+
